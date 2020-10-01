158bff5a095dccf33cc947788cbbca030202de3c
zingzincoachtay <49378571+zingzincoachtay@users.noreply.github.com>
2020-10-01 13:03:36 -0400

    merge

--- sbc-tinkerboardos.sh
+++ sbc-tinkerboardos.sh
@@ -3,8 +3,5 @@
 <<<<<<< HEAD
 
 sudo apt-get purge  scratch*  gpicview  leafpad  galculator  gpicview 
-=======
-sudo apt-get purge  scratch*  gpicview  leafpad  galculator
->>>>>>> c4c62d0b23f701892ec5c40392aa5cd3170a2dec
 
 sudo apt-get autoremove && sudo apt-get autoclean
