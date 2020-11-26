ae6c8076f71db7438d62c218fc62c9187e0d2da0
Tay <kotei.aoki@gmail.com>
2020-11-25 22:31:16 -0500

    adding my go-to package

--- main.sh
+++ main.sh
@@ -11,8 +11,8 @@ sudo apt-get purge libreoffice* claws-mail* dillo epiphany-browser* thunderbird*
 
 sudo apt-get install -y  vim fdupes
 sudo apt-get install -y  exfat-fuse exfat-utils zfsutils-linux ntfs-3g #  afs  zeroconf?
-sudo apt-get install -y  samba xrdp dnsmasq
-sudo apt-get install -y  ffmpeg youtube-dl
+sudo apt-get install -y  samba xrdp dnsmasq nitroshare
+sudo apt-get install -y  ffmpeg youtube-dl atom
 
 #sudo apt-get upgrade -y && sudo apt-get autoremove && sudo apt-get autoclean
 sudo apt-get autoremove && sudo apt-get autoclean
