f8fef4b34cc509bec6da7f37b48ff904a311e4a2
Tay <kotei.aoki@gmail.com>
2020-11-26 21:55:37 -0500

    ready for clean raspbian

--- usb.sh
+++ usb.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sudo hostname blackberry
+cp --backup=numbered /etc/wpa_supplicant/wpa_supplicant.conf ~/Desktop/
+sudo cp main.01.wpa_supplicant /etc/wpa_supplicant/wpa_supplicant.conf
 
 git config --global user.name "Tay"
 git config --global user.email "kotei.aoki@gmail.com"
