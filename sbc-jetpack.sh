c2475dbcfcbdf2b6cf82b7d0f5a1d37cf365d8a3
Tay <kotei.aoki@gmail.com>
2020-11-27 12:25:07 -0500

    ready for jetson

--- sbc-jetpack.sh
+++ sbc-jetpack.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-sudo hostname jetson
+sudo hostname nano
 cp --backup=numbered /etc/wpa_supplicant.conf ~/Desktop/
-sudo cat main.01.wpa_supplicant >> /etc/wpa_supplicant/wpa_supplicant.conf
+#sudo cat main.01.wpa_supplicant >> /etc/wpa_supplicant.conf
 
 bash ./main.sh
-#sudo apt-get purge wolfram-engine gpicview chromium-browser* omxplayer qpdfview* vlc*
-sudo apt-get purge bluej geany* greenfoot scratch* sonic*
-sudo apt-get purge minecraft-pi
+sudo apt-get purge -y thunderbird*
 
 sudo apt-get autoremove && sudo apt-get autoclean
